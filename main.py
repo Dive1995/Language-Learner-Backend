934fdd6f27a3080e0b03c01a744413c327a9a515
Diveshan <diveshan1995@gmail.com>
2024-03-07 06:22:07 +0530

    added multi language and multi code support for transcript

--- main.py
+++ main.py
@@ -9,39 +9,81 @@ app = Flask(__name__)
 CORS(app) 
 api = Api(app)
 
+class LangInput:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
 video_post_args = reqparse.RequestParser()
 video_post_args.add_argument("video_id", type=str, help="Video id missing", required=True)
-video_post_args.add_argument("lang", type=str, help="Language missing", required=True)
+video_post_args.add_argument("lang", type=dict, help="Language missing", required=True) #FIXME: create a new object class with relevant properties and replace the object type
 
 class YotubeVideo(Resource):
     def post(self):
         args = video_post_args.parse_args()
-        transcript = None
+        transcript1 = None
+        transcript2 = None
         print(args["video_id"])
+        lang_input = args["lang"]
+
+        first_langs = lang_input.get('first')
+        print("First: ", first_langs)
+        second_langs = lang_input.get('second')
+        print("Secon: ", second_langs)
+
 
         try:
             transcript_list = YouTubeTranscriptApi.list_transcripts(args["video_id"])
             
             try:
-                manual_transcript = transcript_list.find_manually_created_transcript([args["lang"]])  
-                print("manual ",manual_transcript)
-                transcript = manual_transcript
+                manual_transcript1 = transcript_list.find_transcript(first_langs)  
+                print("manual1 ",manual_transcript1)
+                transcript1 = manual_transcript1
 
             except Exception as e:
-                print(f"Excepption thrown no manual transcripts: {e}")
+                print(f"Excepption thrown no transcripts for first: {e}")
+
+            try:                
+                manual_transcript2 = transcript_list.find_transcript(second_langs)  
+                print("manual2 ",manual_transcript2)
+                transcript2 = manual_transcript2
+
+            except Exception as e:
+                print(f"Excepption thrown no transcripts for second: {e}")
+
+            if transcript1 or transcript2:
+                if(transcript1 is None):
+                    print("1 is none")
+                    transcript1 = transcript2.translate(first_langs)
+
+                if(transcript2 is None):
+                    print("2 is none ")
+                    print("2 is none ", transcript1.translate(second_langs[0]))
+                    transcript2 = transcript1.translate(second_langs[0])
             
-            if transcript is None:
-                try:
-                    generated_transcript = transcript_list.find_generated_transcript([args["lang"]])
-                    print("generated ",generated_transcript)  
-                    transcript = generated_transcript
-                except Exception as e:
-                    print(f"Exception thrown no generated transcripts: {e}")
+            # if transcript1 is None:
+            #     try:
+            #         generated_transcript1 = transcript_list.find_generated_transcript(first_langs)
+            #         print("generated1 ",generated_transcript1)  
+            #         transcript1 = generated_transcript1
+            #     except Exception as e:
+            #         print(f"Exception thrown no generated1 transcripts: {e}")
+
+            # if transcript2 is None:
+            #     try:
+            #         generated_transcript2 = transcript_list.find_generated_transcript(second_langs)
+            #         print("generated2 ",generated_transcript2)  
+            #         transcript2 = generated_transcript2
+            #     except Exception as e:
+            #         print(f"Exception thrown no generated2 transcripts: {e}")
 
         except Exception as e:
             print(f"Programmatic exception thrown: {e}")
+
+        print("first ", transcript1)
+        print("second ", transcript2)
         
-        return {"transcript": None, "is_generated": None} if transcript is None else {"transcript":transcript.fetch(), "is_generated":transcript.is_generated}
+        return {"firstTranscript":None, "first_is_generated":None, "secondTranscript":None, "second_is_generated":None} if transcript1 and transcript2 is None else {"firstTranscript":transcript1.fetch(), "first_is_generated":transcript1.is_generated, "secondTranscript":transcript2.fetch(), "second_is_generated":transcript2.is_generated}
         
 
 
--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.get(BASE + "video", {"video_id":"QDLLgV2fgx4", "lang":"de"})
-response = requests.post(BASE + "vocabulary/context", {"word":"nichts"})
+response = requests.post(BASE + "video", json={"video_id":"uF_pY__Hgu0", "lang": {
+        "first": ["de", "de-DE"],
+        "second": ["en"]
+    }})
+# response = requests.post(BASE + "vocabulary/context", {"word":"nichts"})
 # response = requests.post(BASE + "vocabulary/translation", {"input":"Hund", "to":"de", "from":"en"})
-# response = requests.post(BASE + "video", {"video_id":"dBF65_kHdNg", "lang":"de"})
+# response = requests.get(BASE + "video", {"video_id":"dBF65_kHdNg", "lang":"de"})
 print(response.json())
 
 
